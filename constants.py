d3befb99558ce96ff56d3bcd6a90d5bf057bd509
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2022-12-19 18:55:22 +0530

    refactored. added database.

---
+++ constants.py
@@ -0,0 +1,11 @@
+import datetime
+
+NOW = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+APP_NAME = "QuickreadAI"
+BOOK_DATA = "static/non_fiction_data.json"
+CSS_PATH = "static/style.css"
+
+CACHE_MAX_ENTRIES = 10
+
+# Deta
+SUMMARY_DB = "quickreadai_summary"
