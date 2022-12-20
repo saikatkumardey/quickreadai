efbefc1b35e70a0702976c695959613ada931114
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2022-12-20 11:38:33 +0530

    layout

--- app.py
+++ app.py
@@ -104,6 +104,7 @@ def add_footer():
     """Add footer"""
     _, mid, _ = st.columns([1, 3, 1])
     with mid:
+        add_vertical_space(2)
         st.markdown(
             """<span id="twitter-link"> ❤️ Built by <a href="https://twitter.com/deysaikatkumar" target="_blank">Saikat Kumar Dey</a> | Got <a href="https://forms.gle/arGBqfLNphxrwz6k7">feedback?</a></span>""",
             unsafe_allow_html=True,
@@ -113,40 +114,39 @@ def add_footer():
 def main():
     df = get_book_data()
 
-    _, mid, _ = st.columns([1, 3, 1])
+    _, mid, _ = st.columns([1, 5, 1])
     with mid:
         st.title(f"📚 {APP_NAME}")
-    add_vertical_space(2)
-    st.markdown(
-        "Skim through the core ideas of non-fiction books in 2 minutes."
-    )
-    title_author = st.selectbox(
-        "**Select the book 👇🏽**",
-        df.title_author,
-        help="Select a book from the list to generate summary",
-        label_visibility="collapsed",
-    )
-    title, authors = [item.strip() for item in title_author.split("|")]
-
-    if st.button("Get Summary", type="primary"):
-        st.session_state["title"] = title
-        st.session_state["authors"] = authors
-        with st.spinner(
-            f"Please wait. Getting summary for **{title}** by {authors}..."
-        ):
-            try:
-                st.session_state["output"] = get_summary(
-                    title=title, authors=authors
-                )
-                if not st.session_state["output"]:
-                    st.session_state["output"] = ai.get_gpt_response(
+        st.markdown(
+            "Skim through the core ideas of non-fiction books in 2 minutes."
+        )
+        add_vertical_space(1)
+        title_author = st.selectbox(
+            "**Select the book 👇🏽**",
+            df.title_author,
+            help="Select a book from the list to generate summary",
+            label_visibility="collapsed",
+        )
+        title, authors = [item.strip() for item in title_author.split("|")]
+        if st.button("Get Summary", type="primary"):
+            st.session_state["title"] = title
+            st.session_state["authors"] = authors
+            with st.spinner(
+                f"Please wait. Getting summary for **{title}** by {authors}..."
+            ):
+                try:
+                    st.session_state["output"] = get_summary(
                         title=title, authors=authors
                     )
-                    insert_summary(st.session_state["output"])
-            except Exception as ex:
-                st.error("Error generating summary, please try later..")
+                    if not st.session_state["output"]:
+                        st.session_state["output"] = ai.get_gpt_response(
+                            title=title, authors=authors
+                        )
+                        insert_summary(st.session_state["output"])
+                except Exception as ex:
+                    st.error("Error generating summary, please try later..")
 
-    output_section()
+            output_section()
     add_footer()
 
 
