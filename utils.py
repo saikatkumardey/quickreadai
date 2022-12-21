fa35c37c7a264f8975b1e00362ecefbb06ac1edf
Saikat Kumar Dey <deysaikatkumar@gmail.com>
2022-12-22 00:02:53 +0530

    remove pre-commit and fix utils

--- utils.py
+++ utils.py
@@ -9,19 +9,23 @@ def dump_json(response, path="data/response.json"):
         json.dump(response, fp)
 
 
-def dump_text(text):
-    with open(f"data/prompt-{NOW}", "w") as fp:
+def dump_text(text, path):
+    with open(path, "w") as fp:
         fp.write(text)
 
 
-def local_css(file_name):
-    with open(file_name) as f:
+def local_css(path):
+    with open(path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
 def remote_css(url):
-    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+    st.markdown(
+        f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True
+    )
 
 
 def icon(icon_name):
-    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+    st.markdown(
+        f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True
+    )
